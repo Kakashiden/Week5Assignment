1daddb7fffd620614674b802c523a67d89cd671a
Kakashiden <bhiner1@live.com>
2024-02-10 22:32:30 -0700

    final commit

--- week5Assignment/src/com/promineotech/App.java
+++ week5Assignment/src/com/promineotech/App.java
@@ -1,27 +1,27 @@
-package com.promineotech;
+package com.promineotech; // package
 
-public class App {
+public class App { //class
 
-	public static void main(String[] args) {
-		Logger asterisk = new AsteriskLogger();
-		asterisk.log("Hello");
+	public static void main(String[] args) { //main method
+		Logger asterisk = new AsteriskLogger(); //instantiates a logger of the class AsteriskLogger
+		asterisk.log("Hello"); //passes string hello into logger
 		
-		System.out.println();
+		System.out.println(); //prints space between lines
 		
-		Logger error = new AsteriskLogger();
-		asterisk.error("WARNING");
+		Logger error = new AsteriskLogger(); //instantiates a logger of the class AsteriskLogger
+		asterisk.error("WARNING"); //passes string WARNING into logger
 		
-		System.out.println();
+		System.out.println(); //prints space between lines
 
-		Logger asteriskSpaced = new SpacedLogger();
-		asteriskSpaced.log("Hello");
+		Logger asteriskSpaced = new SpacedLogger(); //instantiates a logger of the class SpacedLogger
+		asteriskSpaced.log("Hello"); //passes string hello into logger
 		
-		System.out.println();
+		System.out.println(); //prints space between lines
 		
-		Logger errorSpaced = new SpacedLogger();
-		asteriskSpaced.error("WARNING");
+		Logger errorSpaced = new SpacedLogger(); //instantiates a logger of the class SpacedLogger
+		asteriskSpaced.error("WARNING"); //passes string WARNING into logger
 		
-		System.out.println();
+		System.out.println(); //prints space between lines
 	}
 
 }
